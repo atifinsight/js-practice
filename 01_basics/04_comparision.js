c7ef5b6dedfb90b8ee6e3ddfff04ef10637614be
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 13:53:56 +0000

    updated

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -21,4 +21,40 @@
 
 // === Strict Check datatype and value both
 
-console.log("2" === 2);
+// console.log("2" === 2);
+
+
+// ***********************
+
+
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// both have hold different values because symbol make them unique
+const id = Symbol('123')
+const id2 = Symbol('123')
+
+// console.log(id === id2)
+
+
+
+// Reference (Non Premitive) their datatype is object
+
+// Array, Objects, Functions
+
+//Array
+const heros = ["Iron Man", "Thor", "Hulk"]
+
+//Objects
+let myObj = {
+    name: "Atif",
+    age: 22,
+}
+
+//function
+const myFunction = function () {
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
