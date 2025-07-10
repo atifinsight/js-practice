a15f57c82db95e7d873f5bc56eaac92a01c87a34
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 15:15:48 +0000

    date

---
+++ 01_basics/07_date.js
@@ -0,0 +1,18 @@
+let myDate = new Date()
+
+//Thu Jul 10 2025 15:05:42 GMT+0000 (Coordinated Universal Time)
+console.log(myDate.toString())
+
+//2025-07-10T15:05:42.824Z
+console.log(myDate.toJSON())
+
+//Thu Jul 10 2025
+console.log(myDate.toDateString())
+
+
+console.log(myDate.toISOString())
+
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString)
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
