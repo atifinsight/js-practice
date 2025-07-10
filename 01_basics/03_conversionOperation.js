cf2be7ad5ffe90ec9d453067004c9e9a0127342a
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 13:12:24 +0000

    conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Atif"
+let score1 = null
+let score2 = undefined
+
+let valueInNumber = Number (score)
+let valueInNumber1 = Number (score1)
+let valueInNumber2 = Number (score2)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(valueInNumber1);
+// console.log(valueInNumber2);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true/false => 1/0
+
+
+let isLoggedIn = "a"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn)
+
+// 1=>true; 0=>false
+// "" => false
+// "aa" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
