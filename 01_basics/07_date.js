abff9eebd16f398ff2ac964d51514c9d2ada92d8
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 15:26:15 +0000

    update

--- 01_basics/07_date.js
+++ 01_basics/07_date.js
@@ -1,18 +1,38 @@
 let myDate = new Date()
 
 //Thu Jul 10 2025 15:05:42 GMT+0000 (Coordinated Universal Time)
-console.log(myDate.toString())
+// console.log(myDate.toString())
 
-//2025-07-10T15:05:42.824Z
-console.log(myDate.toJSON())
+// //2025-07-10T15:05:42.824Z
+// console.log(myDate.toJSON())
 
-//Thu Jul 10 2025
-console.log(myDate.toDateString())
+// //Thu Jul 10 2025
+// console.log(myDate.toDateString())
 
+// //2025-07-10T15:15:37.930Z
+// console.log(myDate.toISOString())
 
-console.log(myDate.toISOString())
+// //7/10/2025
+// console.log(myDate.toLocaleDateString())
 
-console.log(myDate.toLocaleDateString())
-console.log(myDate.toLocaleString)
-console.log(myDate.toLocaleTimeString())
-console.log(myDate.toTimeString())
+// //3:15:37 PM
+// console.log(myDate.toLocaleTimeString())
+
+// //15:15:37 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toTimeString())
+
+let myCreatedDate = new Date("01-14-2023")
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
+console.log(newDate.getDate())
