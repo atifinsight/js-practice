98065db47dda4103e555c587b23959f9283f9b67
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 12:50:38 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 123
+let accountEmail = "atif123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore"
+let accountState;
+
+// accountID = 2   // not allowed
+
+
+// accountEmail = "qasim1234@gmail.com"  // allowed
+// accountPassword = "00000"  //allowed
+// accountCity = "RWP"     //allowed
+
+console.log(accountEmail);
+
+/*
+
+*/
+
+console.table([accountID, accountEmail ,accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version of JS
+
+//alert("Hello")  // we are using nodejs, not browser
+
+let name = "Atif"  // sting
+let age = 20  // numebr
+let isLoggedIn = false  // boolean
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // it's object type
+// undefined => // undefined type
+// symbol => 
+// object
+
+console.log(typeof undefined);  //undfined
+console.log(typeof null);   //object
