909772ec0fabbf37e068e0b1b25cb15d4edc7426
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 13:36:47 +0000

    done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log("2" > 1)
+
+// console.log("02" > 1)
+
+
+// console.log(null == 0)
+
+// Comparision converts null to a number, treating it as 0.
+// console.log(null < 0)
+// console.log(null > 0)
+// console.log(null <= 0)
+// console.log(null >= 0)
+
+
+// all false value
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined <= 0)
+// console.log(undefined >= 0)
+
+// === Strict Check datatype and value both
+
+console.log("2" === 2);
