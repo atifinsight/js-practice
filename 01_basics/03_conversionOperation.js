25851f4aef2821f6de5e2235e807126f6536d5e2
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-10 13:26:46 +0000

    updated

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -29,5 +29,28 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+// *********************** Operations ************************
+
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Atif"
+
+str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2+ 2);
+// console.log(1 + 2+ "2");
+
+// console.log((1 + 2) * (2 % 4) / 8);
+
+
