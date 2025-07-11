ab4c13b673e50f93c1d73adfbfd1ed69d7948142
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-11 14:52:32 +0000

    scope update

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -10,7 +10,7 @@ if (true) {
 if (true) {
     let a = 10
     const b =20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
 }
 
@@ -19,6 +19,46 @@ if (true) {
     
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "atif"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username)
+    }
+    //console.log(website);
+    two()
+    
+}
+one()
+
+
+if (true) {
+    const username = "atif"
+    if (username === "atif") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// ************************ Interesting ************************
+// Both are syntax of declaring function but different limitations
+
+console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
+
