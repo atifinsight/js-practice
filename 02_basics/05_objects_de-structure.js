a50552823c59339f5ce55f86b6679f9eea2918c9
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-11 12:02:48 +0000

    object destructure

---
+++ 02_basics/05_objects_de-structure.js
@@ -0,0 +1,12 @@
+const course = {
+    courseName: "JS in english",
+    price: "999",
+    courseInstrcutor: "Atif"
+}
+
+// course.courseInstrcutor
+
+const {courseInstrcutor: instructor} = course
+
+// console.log(courseInstrcutor);
+console.log(instructor);
