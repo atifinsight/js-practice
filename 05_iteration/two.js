629ffe633bed532b30a2e556f29101bd0bb77050
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-13 17:48:26 +0000

    while loop

---
+++ 05_iteration/two.js
@@ -0,0 +1,13 @@
+let index = 0
+while (index <= 10) {
+    // console.log(`Values of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["atif", "imran", "soban"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
