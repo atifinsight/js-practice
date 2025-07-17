07b51b5f7595215d43399bb351d3c15ac7dcb8fb
Muhammad Atif <72624532+muhammadatif090@users.noreply.github.com>
2025-07-17 01:55:25 +0000

    2-BMI cal update

--- 07_projects/2-BMICalculator/atifinsight.js
+++ 07_projects/2-BMICalculator/atifinsight.js
@@ -9,13 +9,29 @@ form.addEventListener('submit', function(e){
     const height = parseInt(document.querySelector('#height').value)
     const weight = parseInt(document.querySelector('#weight').value)
     const results = document.querySelector('#results')
+    const final_weight = document.querySelector('#final_weight')
     if (height === '' || height < 0 || isNaN(height)) {
         results.innerHTML = `Please give a valid height ${height}`
     } else if(weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = `Please give a valid height ${weight}`
+        results.innerHTML = `Please give a valid weight ${weight}`
     } else{
         const bmi = (weight / ((height*height)/10000)).toFixed(2)
         // show the result
         results.innerHTML = `<span>${bmi}</span>`
+        
+        const bmiValue = parseFloat(bmi)  // Convert back to number
+
+
+        if (bmiValue < 18.6) {
+            final_weight.innerHTML = '<span>Under Weight</span>'
+        } else if(bmiValue > 18.6 && bmiValue < 24.9){
+            final_weight.innerHTML = '<span>Normal Weight</span>'
+        } else if(bmiValue > 24.9){
+            final_weight.innerHTML = '<span>Over Weight</span>'
+        } else {
+            final_weight.innerHTML = '<span>Not in range please give correct height and weight</span>'
+        }
+    
     }
+
 })
